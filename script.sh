aaff72732d9037d22dce9c23ac6a44e27aeda942
Marlon Rodrigues <42067419+M4rlon-R0drigues@users.noreply.github.com>
2019-09-27 09:04:11 -0400

    File upload
    Envio do arquivos script.sh e tutorial de execução.
    Uploading script.sh files and tutorial de execução.

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo su
+#<enter the root password>
+cd
+apt update
+apt upgrade
+#<command for installing the programs>
+apt install -f 
+exit
+
+#script for update of repositories and upgrade of programs
+#script para atualização de repositórios e atualização de programas
